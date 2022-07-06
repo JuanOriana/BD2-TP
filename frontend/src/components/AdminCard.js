b30766e99fa6d7ea15a2a32c2a68aa7fc92b9c4a
Paula Domingues <pdomingues@itba.edu.ar>
2022-07-06 01:49:58 -0300

    feat: admin cards

---
+++ frontend/src/components/AdminCard.js
@@ -0,0 +1,33 @@
+import {Box, Button, Center, Heading, Stack, Text, useColorModeValue} from "@chakra-ui/react";
+import {Icon} from "@chakra-ui/icons";
+
+const AdminCard = ({icon, title, description}) => {
+  return <>
+    <Button
+      role={'group'}
+      p={6}
+      maxW={'330px'}
+      w={'full'}
+      h={'full'}
+      bg={useColorModeValue('whiteAlpha', 'gray.900')}
+      boxShadow={'xl'}
+      rounded={'lg'}
+      pos={'relative'}
+      onClick={() => {}}
+      >
+      <Stack align={'center'}>
+        <Icon as={icon} w={10} h={10}/>
+        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
+          {title}
+        </Heading>
+        <Stack direction={'row'} align={'center'}>
+          <Text color={useColorModeValue('gray.600', 'gray.400')}>
+            {description}
+          </Text>
+        </Stack>
+      </Stack>
+    </Button>
+  </>;
+}
+
+export default AdminCard;
