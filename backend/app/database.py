e5adcf3dfc70ccc578c9c2f3f4233a5b025c6002
Reversive <matiaspavan@gmail.com>
2022-07-09 13:42:01 -0300

    feat: redis and mongo integrations + composer

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,11 @@
+import redis
+import pymongo
+from app.core.config import settings
+from typing import Any
+
+redis_client = redis.Redis(host=settings.REDIS_HOSTNAME, port=settings.REDIS_PORT, db=0)
+mongo_client = pymongo.MongoClient(settings.MONGODB_URL)
+
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
