bb5d62e10f56f98ff350123b78e0814433dcf9ab
Reversive <matiaspavan@gmail.com>
2022-07-09 17:33:28 -0300

    feat: plan endpoint logic

--- backend/app/api/plans.py
+++ backend/app/api/plans.py
@@ -1,11 +1,26 @@
 from fastapi import APIRouter, HTTPException
-from app.core.schemas.plan import Plan
+from app.core.schemas.plan import Plan, ExtPlan
+from app.database import plan_collection
+from bson.objectid import ObjectId
+
 router = APIRouter()
 
-@router.get("", response_model=list[Plan], status_code=200)
+@router.get("", response_model=list[ExtPlan], status_code=200)
 async def get_all_plans():
-    return []
+    # get all plans from plan_collection with their corresponding object id and append it to a list
+    plans = []
+    for plan in plan_collection.find():
+        ext_plan = ExtPlan(**plan)
+        ext_plan.id = str(plan["_id"])
+        plans.append(ext_plan)
+    return plans
 
-@router.get("/{plan_id}", response_model=list[Plan], status_code=200)
-async def get_all_plans(plan_id : int):
-    return {"plan_id": plan_id, "expiration_days": 30, "max_url_count": 5000} 
+
+
+
+@router.get("/{plan_id}", response_model=Plan, status_code=200)
+async def get_all_plans(plan_id : str):
+    plan = plan_collection.find_one({"_id": ObjectId(plan_id)})
+    if not plan:
+        raise HTTPException(status_code=404, detail="Plan not found")
+    return Plan(**plan)
--- backend/app/core/schemas/plan.py
+++ backend/app/core/schemas/plan.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel
+from typing import Union
+
 class Plan(BaseModel):
-    plan_id: int
+    name: str
+    price: float
     expiration_days: int
     max_url_count: int
+
+class ExtPlan(Plan):
+    id: Union[str, None] = None
