45a221415eb2258eb31dbb763d6ac1d37aec4687
Reversive <matiaspavan@gmail.com>
2022-07-09 15:42:20 -0300

    feat: user registration logic

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -4,7 +4,12 @@ from app.core.schemas.user import User, UserInDB
 from app.core.schemas.url import URLInfo
 from app.core.schemas.plan import Plan
 from app.database import fake_users_db
+from app.core.models.user_register import UserRegister
+from app.database import user_collection
+from passlib.context import CryptContext
+from jose import JWTError, jwt
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 router = APIRouter()
 
@@ -55,6 +60,25 @@ async def get_user_links_by_username(username: str):
 @router.get("/{username}/plan", response_model=Plan, status_code=200)
 async def get_user_plan_by_username(username: str):
     return {"plan_id": 1, "expiration_days": 30, "max_url_count": 5000} 
+
+# POSTs
+
+@router.post("", status_code=201)
+async def register_user(user: UserRegister):
+    new_user = {
+        "username": user.username,
+        "email": user.email,
+        "password": pwd_context.hash(user.password)
+    }
+    user_exists = user_collection.find_one({"username": new_user["username"]})
+    email_exists = user_collection.find_one({"email": new_user["email"]})
+
+    if not user_exists and not email_exists:
+	    user_collection.insert_one(new_user)
+	    return {}
+    else:
+	    raise HTTPException(status_code=409, detail="Username/email already exists")
+
 
 # DELETEs
 @router.delete("/{username}", status_code=204)
--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,11 @@ from app.core.config import settings
 from typing import Any
 
 redis_client = redis.Redis(host=settings.REDIS_HOSTNAME, port=settings.REDIS_PORT, db=0, password=settings.REDIS_PASSWORD)
+
+
 mongo_client = pymongo.MongoClient(settings.MONGODB_URL)
+mongo_db = mongo_client["shawty"]
+user_collection = mongo_db["users"]
 
 fake_users_db = {
     "johndoe": {
