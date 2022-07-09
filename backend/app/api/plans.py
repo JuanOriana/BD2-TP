45dcaa31098ef694f4cf9c74bffb132c918e0b3f
Reversive <matiaspavan@gmail.com>
2022-07-09 18:38:36 -0300

    refactor: plan endpoints

--- backend/app/api/plans.py
+++ backend/app/api/plans.py
@@ -1,13 +1,19 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 from app.core.schemas.plan import Plan, ExtPlan
 from app.database import plan_collection
 from bson.objectid import ObjectId
 
 router = APIRouter()
 
-@router.get("", response_model=list[ExtPlan], status_code=200)
+def get_standard_plan():
+    return plan_collection.find_one({"name": "Standard"})
+
+@router.get(
+        "", 
+        response_model = list[ExtPlan], 
+        status_code = status.HTTP_200_OK
+    )
 async def get_all_plans():
-    # get all plans from plan_collection with their corresponding object id and append it to a list
     plans = []
     for plan in plan_collection.find():
         ext_plan = ExtPlan(**plan)
@@ -15,11 +21,13 @@ async def get_all_plans():
         plans.append(ext_plan)
     return plans
 
-
-
-
-@router.get("/{plan_id}", response_model=Plan, status_code=200)
-async def get_all_plans(plan_id : str):
+@router.get(
+        "/{plan_id}", 
+        response_model = Plan, 
+        status_code = status.HTTP_200_OK)
+async def get_all_plans(
+        plan_id : str
+    ):
     plan = plan_collection.find_one({"_id": ObjectId(plan_id)})
     if not plan:
         raise HTTPException(status_code=404, detail="Plan not found")
