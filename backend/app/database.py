40270e6564eb95d8480af4bd2c4eba29e9caf9a8
Reversive <matiaspavan@gmail.com>
2022-07-09 17:26:47 -0300

    feat: db connection check and default plan population

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,24 +5,23 @@ from typing import Any
 
 redis_client = redis.Redis(host=settings.REDIS_HOSTNAME, port=settings.REDIS_PORT, db=0, password=settings.REDIS_PASSWORD)
 
-
 mongo_client = pymongo.MongoClient(settings.MONGODB_URL)
 mongo_db = mongo_client["shawty"]
 user_collection = mongo_db["users"]
+link_collection = mongo_db["links"]
+plan_collection = mongo_db["plans"]
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
+try:
+    redis_client.ping()
+except redis.exceptions.ConnectionError:
+    raise Exception("Redis connection error")
+
+try:
+    mongo_client.list_database_names()
+except pymongo.errors.ConnectionFailure:
+    raise Exception("Mongo connection error")
+
+if not plan_collection.count_documents({}):
+    plan_collection.insert_one({"name": "Standard", "price": 0, "expiration_days": 30, "max_url_count": 5000})
+    plan_collection.insert_one({"name": "Premium", "price": 14.99, "expiration_days": 90, "max_url_count": 10000})
+
