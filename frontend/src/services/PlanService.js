aeb513aec5617ac03c2290180b5c39cb7e99132f
JuanOriana <joriana@itba.edu.ar>
2022-07-10 22:20:17 -0300

    feat: plan service

---
+++ frontend/src/services/PlanService.js
@@ -0,0 +1,16 @@
+import { APPLICATION_V1_JSON_TYPE, paths } from "../common/constants";
+
+import { resultFetch } from "../scripts/resultFetch";
+const basePath = paths.BASE_URL + paths.PLANS;
+
+export class PlanService {
+  async getPlans(page, pageSize) {
+    return resultFetch(basePath);
+  }
+
+  async getPlanById(id) {
+    return resultFetch(basePath + "/" + id, {
+      method: "GET",
+    });
+  }
+}
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,7 +1,11 @@
 import { UserService } from "./UserService";
 import { TokenService } from "./TokenService";
+import { LinkService } from "./LinkService";
+import { PlanService } from "./PlanService";
 
 const userService = new UserService();
 const tokenService = new TokenService();
+const linkService = new LinkService();
+const planService = new PlanService();
 
-export { userService, tokenService };
+export { userService, tokenService, linkService, planService };
